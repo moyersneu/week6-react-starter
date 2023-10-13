a57c5cb9ec711af121dd473d52bd668710bacb6d
smoyer <moyer.s@northeastern.edu>
2023-10-13 14:17:47 -0400

    react test added

--- src/calculator.spec.tsx
+++ src/calculator.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Calculator from './calculator';
 
 describe('Calculator', () => {
@@ -8,6 +8,22 @@ describe('Calculator', () => {
     render(<Calculator />);
     const linkElement = screen.getByText(/1/i);
     expect(linkElement).toBeInTheDocument();
+  });
+
+  it('Calculator displays "4" after pressing "1 + 3 =', () => {
+    const { getByText } = render(<Calculator />);
+  
+    // Press NumericKey.ONE
+    fireEvent.click(getByText(/1/));
+    // Press OperatorKey.PLUS
+    fireEvent.click(getByText(/+/));
+    // Press NumericKey.THREE
+    fireEvent.click(getByText(/3/));
+    // Press ActionKey.EQUALS
+    fireEvent.click(getByText(/=/));
+  
+    // Check if display shows "4"
+    expect(getByText(/4/)).toBeInTheDocument();
   });
 
 });
